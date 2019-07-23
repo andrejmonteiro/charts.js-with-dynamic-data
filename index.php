a64cf912d34ba19f2698f2a6d96208f93a61b361
André Monteiro <andrejmonteiro96@outlook.pt>
2019-07-23 01:17:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="utf-8">
+		<meta http-equiv="refresh" content="300">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+  </head>
+  <body>
+    <script src="assets/js/"></script>
+  </body>
+</html>
